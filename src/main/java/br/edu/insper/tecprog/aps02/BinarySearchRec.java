ad3a9cf8819826e63c906bdbf6d97775dc6dac5c
Igor Montagner <igordsm@gmail.com>
2023-03-01 10:18:11 -0300

    testes busca recursiva

---
+++ src/main/java/br/edu/insper/tecprog/aps02/BinarySearchRec.java
@@ -0,0 +1,9 @@
+package br.edu.insper.tecprog.aps02;
+
+import br.edu.insper.tecprog.aps01.InsperList;
+
+public class BinarySearchRec {
+    public static <T extends Comparable<T>> int buscaBinaria(InsperList<T> l, T value) {
+        // TODO: completar aqui
+    }
+}
