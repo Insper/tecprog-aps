35b4a3d17d21d29f1ee3534a98c942d4aa10ba27
Igor Montagner <igordsm@gmail.com>
2023-03-01 10:03:24 -0300

    Testes para busca binária

--- src/test/java/br/edu/insper/tecprog/aps02/TestBinarySearch.java
+++ src/test/java/br/edu/insper/tecprog/aps02/TestBinarySearch.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
@@ -71,27 +72,40 @@ public class TestBinarySearch {
         assertEquals(0, BinarySearchIter.<String>buscaBinaria(l, "BLABLA"));
     }
 
+    public Object[] peekObjectArray(InsperList l) throws Exception {
+        var atr = l.getClass().getDeclaredField("data");
+        atr.setAccessible(true);
+        var actual_data = (Object[]) atr.get(l);
+        return actual_data;
+    }
+
+
+
     @Test
-    void buscaListaEncontraTodos() {
+    void buscaListaEncontraTodos() throws Exception {
         var l = new InsperArrayList<String>();
         for (int i = 0; i < 11; i++) {
             l.add("BLABLA" + Integer.toString(i));
         }
+        var data = peekObjectArray(l);
+        Arrays.sort(data, 0, l.size());
         for (int i = 0; i < 11; i++) {
-            assertEquals(i, BinarySearchIter.<String>buscaBinaria(l, "BLABLA" + Integer.toString(i)));
+            var valor = "BLABLA" + Integer.toString(i);
+            assertEquals(Arrays.binarySearch(data, 0, l.size(), valor), BinarySearchIter.<String>buscaBinaria(l, valor));
         }
     }
 
     @Test
     @Timeout(value=1, unit=TimeUnit.SECONDS)
-    void buscaListaEncontraTodosGRANDE() {
-        var l = new InsperArrayList<String>();
-        int N = 10001;
+    void buscaListaEncontraTodosGRANDE() throws Exception {
+        var l = new InsperArrayList<Double>();
+        int N = 100001;
         for (int i = 0; i < N; i++) {
-            l.add("BLABLA" + Integer.toString(i));
+            l.add(0.1 * i);
         }
         for (int i = 0; i < N; i++) {
-            assertEquals(i, BinarySearchIter.<String>buscaBinaria(l, "BLABLA" + Integer.toString(i)));
+            var valor = 0.1 * i;
+            assertEquals(i, BinarySearchIter.<Double>buscaBinaria(l, valor));
         }
     }
 
