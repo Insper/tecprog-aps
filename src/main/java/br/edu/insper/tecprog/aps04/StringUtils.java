1c7fa15740e665298e9057f4c7330445a8ef284a
Igor Montagner <igordsm@gmail.com>
2023-03-31 10:32:22 -0300

    Mais arquivos para APS04- texto

--- src/main/java/br/edu/insper/tecprog/aps04/StringUtils.java
+++ src/main/java/br/edu/insper/tecprog/aps04/StringUtils.java
@@ -1,5 +1,7 @@
 package br.edu.insper.tecprog.aps04;
 
+import br.edu.insper.tecprog.aps01.InsperArrayList;
+
 public class StringUtils {
 
     public static boolean prefixo(String S, String Q) {
@@ -24,5 +26,17 @@ public class StringUtils {
 
     public static String replaceAll(String S, String Q, String newQ) {
         return S;
+    }
+
+    public static InsperArrayList<String> sumario(String md) {
+        return new InsperArrayList<String>();
+    }
+
+    public static String tiraTags(String html) {
+        return "";
+    }
+
+    public static InsperArrayList<String> emailsInsper(String s) {
+        return new InsperArrayList<String>();
     }
 }
